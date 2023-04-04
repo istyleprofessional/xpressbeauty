50bda170da337b59b71f0089c3474ec00bd22631
ramyanber94 <ramyanber@gmail.com>
2023-04-04 15:16:48 -0400

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import { qwikVite } from '@builder.io/qwik/optimizer';
 import { qwikCity } from '@builder.io/qwik-city/vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig(() => {
   return {
-    plugins: [qwikCity(), qwikVite(), tsconfigPaths()],
+    plugins: [ EnvironmentPlugin(["QWIK_APP_MONGO_CONNECTION", "QWIK_APP_TOKEN_SECRET", "QB_USERNAME", "QB_PASSWORD", "QB_COMPANY_FILE", "QB_SOAP_PORT"]), qwikCity(), qwikVite(), tsconfigPaths()],
     preview: {
       headers: {
         'Cache-Control': 'public, max-age=600',
