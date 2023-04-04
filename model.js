949a98aba25dc3eec17d5d4e10b50c3965ef7cf3
ramyanber94 <ramyanber@gmail.com>
2023-04-04 15:10:52 -0400

    add model for seeder

---
+++ model.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const { mongoose } = require("mongoose");
+
+const productsSchema = new mongoose.Schema(
+  {
+    product_name: { type: String },
+    description: { type: String, default: null },
+    item_no: { type: String },
+    sale_price: { type: String, default: null },
+    regular_price: { type: String },
+    category: { type: String },
+    image: { type: String },
+    wholesale_price: { type: String },
+    wholesale_sale_price: { type: String },
+    quantity_on_hand: { type: String },
+  },
+  { timestamps: true }
+);
+
+const usersSchema = new mongoose.Schema(
+  {
+    firstName: { type: String },
+    secondName: { type: String },
+    email: { type: String },
+    password: { type: String },
+    role: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = {
+  Product: mongoose.model("products", productsSchema),
+  User: mongoose.model("users", usersSchema),
+};
