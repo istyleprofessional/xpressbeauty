5b99dc9e4959204615d8df86bac243aa63700a48
ramyanber94 <ramyanber@gmail.com>
2023-10-18 10:41:45 -0400

    remove all console.log and adjust meta description

--- src/routes/reset-password/index.tsx
+++ src/routes/reset-password/index.tsx
@@ -66,7 +66,6 @@ export default component$(() => {
   const isLoading = useSignal<boolean>(false);
   const message = useSignal<string>("");
   const action = useAction();
-  const isRecaptcha = useSignal<boolean>(false);
   const recaptchaToken = useSignal<string>("");
   const loc = useLocation();
   const token = loc.url.searchParams.get("token") ?? "";
@@ -81,19 +80,15 @@ export default component$(() => {
   });
 
   useVisibleTask$(
-    () => {
-      if (isRecaptcha.value === false) {
-        isRecaptcha.value = true;
-        setTimeout(() => {
-          (window as any).grecaptcha.ready(async () => {
-            const token = await (window as any).grecaptcha.execute(
-              process.env.RECAPTCHA_SITE_KEY ?? "",
-              { action: "submit" }
-            );
-            recaptchaToken.value = token;
-          });
-        }, 1000);
-      }
+    ({ track }) => {
+      track(() => action.value?.status);
+      (window as any).grecaptcha.ready(async () => {
+        const token = await (window as any).grecaptcha.execute(
+          process.env.RECAPTCHA_SITE_KEY ?? "",
+          { action: "submit" }
+        );
+        recaptchaToken.value = token;
+      });
     },
     { strategy: "document-idle" }
   );
@@ -116,12 +111,10 @@ export default component$(() => {
         <div class="w-full h-96 bg-no-repeat md:hidden bg-[url('/Registration.webp')] bg-contain bg-center">
           {" "}
         </div>
-        {isRecaptcha.value === true && (
-          <script
-            src={`https://www.google.com/recaptcha/api.js?render=${process.env.RECAPTCHA_SITE_KEY}`}
-          ></script>
-        )}
-        <Form action={action} reloadDocument={true}>
+        <script
+          src={`https://www.google.com/recaptcha/api.js?render=${process.env.RECAPTCHA_SITE_KEY}`}
+        ></script>
+        <Form action={action} reloadDocument={false}>
           <div class="card w-[90%] md:w-[35rem] h-fit m-6 shadow-xl bg-[#F4F4F5] flex flex-col justify-center items-center gap-5 p-5">
             {message.value && (
               <div class="w-full">
