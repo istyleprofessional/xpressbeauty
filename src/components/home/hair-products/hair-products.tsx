c69b5f1e63aeab2127d483a6dcff2ddc711ce152
ramyanber94 <ramyanber@gmail.com>
2023-10-26 18:02:46 -0400

    add meta pixel

--- src/components/home/hair-products/hair-products.tsx
+++ src/components/home/hair-products/hair-products.tsx
@@ -1,4 +1,4 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { ProductCard } from "~/components/shared/product-card/product-card";
 import type { ProductModel } from "~/models/product.model";
 
@@ -8,17 +8,30 @@ interface NewArrivalProps {
 
 export const HairProducts = component$((props: NewArrivalProps) => {
   const { newArrivalProducts } = props;
+  const isVisble = useSignal<boolean>(false);
+
+  useVisibleTask$(
+    ({ track }) => {
+      track(() => isVisble.value);
+
+      isVisble.value = true;
+    },
+    { strategy: "intersection-observer" }
+  );
+
   return (
     <div
       class="flex flex-col justify-center items-center gap-7 w-full"
       style="background-image: url(Dash-lines.webp) "
     >
       <h2 class="font-inter font-bold text-4xl text-black">Hair</h2>
-      <div class="m-6 flex flex-row flex-wrap gap-10 justify-center items-center w-full">
-        {newArrivalProducts?.map((item: ProductModel, i: number) => (
-          <ProductCard product={item} i={i} key={i} />
-        ))}
-      </div>
+      {isVisble.value && (
+        <div class="m-6 flex flex-row flex-wrap gap-10 justify-center items-center w-full">
+          {newArrivalProducts?.map((item: ProductModel, i: number) => (
+            <ProductCard product={item} i={i} key={i} />
+          ))}
+        </div>
+      )}
     </div>
   );
 });
