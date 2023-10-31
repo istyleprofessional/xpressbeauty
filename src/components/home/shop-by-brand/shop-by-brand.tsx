4744ebdebc43c7ee826aa5c2dcce58b90da09f60
ramyanber94 <ramyanber@gmail.com>
2023-10-31 15:48:06 -0400

    adjust payment logic

--- src/components/home/shop-by-brand/shop-by-brand.tsx
+++ src/components/home/shop-by-brand/shop-by-brand.tsx
@@ -13,46 +13,55 @@ export const ShopByBrand = component$(() => {
     <div class="flex flex-col justify-center items-center pt-7 lg:pt-20">
       <h1 class="font-bold text-4xl text-black">Shop By Brand</h1>
       <div class="flex flex-row flex-wrap justify-center gap-20 p-10 items-center">
-        <img
-          src="brands-images/Andis-Company.webp"
-          alt="andis logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/Babyliss.webp"
-          alt="Babyliss logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="/brands-images/3VERSINCE-logo.webp"
-          alt="3VERSINCE logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/PHILTHYBLENDZ-logo.webp"
-          alt="PHILTHYBLENDZ logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/Schwarzkopf.webp"
-          alt="Schwarzkopf logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/matrix.webp"
-          alt="andis logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/John-Paul-Mitchell.webp"
-          alt="John Paul Mitchell logo"
-          class="lg:w-60 w-28 h-full"
-        />
-        <img
-          src="brands-images/L'Oréal.webp"
-          alt="L'Oréal logo"
-          class="lg:w-60 w-28 h-full"
-        />
+        <a href="/products/filterBrands/ANDIS/">
+          <img
+            src="brands-images/Andis-Company.webp"
+            alt="andis logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/BABYLISSPRO/">
+          <img
+            src="brands-images/Babyliss.webp"
+            alt="Babyliss logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/3VERSINCE/">
+          <img
+            src="brands-images/3VERSINCE-logo.webp"
+            alt="3VERSINCE logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/PHILTHYBLENDZ/">
+          <img
+            src="brands-images/PHILTHYBLENDZ-logo.webp"
+            alt="PHILTHYBLENDZ logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/Matrix/">
+          <img
+            src="brands-images/matrix.webp"
+            alt="andis logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/John-Paul-Mitchell-Systems/">
+          <img
+            src="brands-images/John-Paul-Mitchell.webp"
+            alt="John Paul Mitchell logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
+        <a href="/products/filterBrands/L'Oreal-Professionnel/">
+          <img
+            src="brands-images/L'Oréal.webp"
+            alt="L'Oréal logo"
+            class="lg:w-60 w-28 h-full"
+          />
+        </a>
       </div>
       <button
         class="btn btn-primary btn-lg text-white font-['Inter'] w-fit rounded-sm mt-8"
