793e1306112fc495f7d3ebc2df36939e9591853b
ramyanber94 <ramyanber@gmail.com>
2023-11-23 14:20:54 -0500

    adjust toolbar

--- src/routes/api/admin/product/update/index.ts
+++ src/routes/api/admin/product/update/index.ts
@@ -2,10 +2,9 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 import { connect } from "~/express/db.connection";
 import { update_product_service } from "~/express/services/product.service";
 
-export const onPut: RequestHandler = async ({ json, parseBody, cookie }) => {
+export const onPut: RequestHandler = async ({ json, parseBody }) => {
   await connect();
   const body = await parseBody();
-  const token = cookie.get("token")?.value;
-  const result = await update_product_service(body, token ?? "");
+  const result = await update_product_service(body);
   json(200, result);
 };
