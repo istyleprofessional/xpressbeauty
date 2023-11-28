0d349063d85b2540285f305f32e850f72cea9bf6
ramyanber94 <ramyanber@gmail.com>
2023-11-28 14:31:48 -0500

    update google sheet

--- src/routes/api/feedShopping/index.ts
+++ src/routes/api/feedShopping/index.ts
@@ -1,21 +1,19 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
 import { JWT } from "google-auth-library";
-import { google } from "googleapis";
+// import { google } from "googleapis";
 import { connect } from "~/express/db.connection";
 import { GoogleSpreadsheet } from "google-spreadsheet";
 import productSchema from "~/express/schemas/product.schema";
+// import { title } from "node:process";
 
 export const onGet: RequestHandler = async ({ json }) => {
   await connect();
-  const merchantId = "5086882223"; // Replace with your merchant ID
+  // const merchantId = "5086882223"; // Replace with your merchant ID
 
   const auth = new JWT({
     email: import.meta.env.VITE_GOOGLE_SERVICE_ACCOUNT_EMAIL ?? "",
     key: import.meta.env.VITE_GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n") ?? "",
-    scopes: [
-      "https://www.googleapis.com/auth/content",
-      "https://www.googleapis.com/auth/spreadsheets",
-    ],
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
   const doc = new GoogleSpreadsheet(
@@ -35,40 +33,63 @@ export const onGet: RequestHandler = async ({ json }) => {
     });
     return jsonRow;
   });
+  const dataToBeAdd: any[] = [];
   for (let i = 0; i < jsonRows.length; i++) {
     // check if product has number in title
+    if (jsonRows[i].id.includes("-")) continue;
     const productFromDb = await productSchema.findOne({ _id: jsonRows[i].id });
     if (!productFromDb) {
       continue;
     }
-
-    if (productFromDb.priceType === "range") {
-      continue;
+    if (productFromDb.variation_type) {
+      for (const variation of productFromDb.variations) {
+        const newDate = {
+          id: `${productFromDb._id}-${variation.variation_id}`,
+          title: `${productFromDb.product_name}-${variation.variation_name}`,
+          description: productFromDb.description,
+          availability:
+            parseInt(variation?.quantity_on_hand?.toString() ?? "0") > 0
+              ? "in_stock"
+              : "out_of_stock",
+          link: `https://xpressbeauty.ca/products/${productFromDb.perfix}`,
+          image_link: productFromDb.imgs[0],
+          price: `${parseFloat(
+            variation?.price?.toString()?.replace("$", "")
+          )?.toFixed(2)} CAD`,
+          identifier_exists: "no",
+          brand: productFromDb.companyName.name ?? "",
+          condition: "new",
+        };
+        dataToBeAdd.push(newDate);
+      }
     }
-
-    try {
-      const content = google.content({
-        version: "v2.1",
-        auth: auth,
-      });
-      const request = await content.products.update({
-        merchantId: merchantId,
-        productId: `online:en:VI:${jsonRows[i].id}` ?? "",
-        requestBody: {
-          //   offerId: jsonRows[i].id ?? "",
-          salePrice: {
-            currency: "CAD",
-            value: (
-              parseFloat(productFromDb.price?.regular?.toString()) -
-              parseFloat(productFromDb.price?.regular?.toString()) * 0.2
-            ).toFixed(2),
-          },
-        },
-      });
-      console.log(request.data);
-    } catch (error: any) {
-      console.log(error);
-    }
+    const newDate: any = {
+      id: productFromDb._id,
+      title: productFromDb.product_name,
+      description: productFromDb.description,
+      availability:
+        parseInt(productFromDb?.quantity_on_hand ?? "0") > 0
+          ? "in_stock"
+          : "out_of_stock",
+      link: `https://xpressbeauty.ca/products/${productFromDb.perfix}`,
+      image_link: productFromDb.imgs[0],
+      price: `${parseFloat(productFromDb?.price?.regular?.toString())?.toFixed(
+        2
+      )} CAD`,
+      sale_price: `${(
+        parseFloat(productFromDb.price?.regular?.toString()) -
+        parseFloat(productFromDb.price?.regular?.toString()) * 0.2
+      ).toFixed(2)} CAD`,
+      identifier_exists: "no",
+      brand: productFromDb.companyName.name,
+      condition: "new",
+    };
+    dataToBeAdd.push(newDate);
   }
-  json(200, { message: "success" });
+  // delete all rows expect header
+  await sheet.clearRows();
+  // add new rows
+  await sheet.addRows(dataToBeAdd);
+
+  json(200, { message: "done" });
 };
