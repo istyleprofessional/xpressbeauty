8d2e77fea91b24cb01374e49bf4aec2ebd334b12
MeghaMehta96 <meghavimehta96@gmail.com>
2023-11-30 11:59:25 -0500

    change in my order

--- src/routes/profile/my-order/[id]/index.tsx
+++ src/routes/profile/my-order/[id]/index.tsx
@@ -25,9 +25,8 @@ export const useGetMyOrderDetails = routeLoader$(
       }
       const data = {
         ...orderDetails.request,
-        userName: `${user.result?.firstName ?? ""} ${
-          user.result?.lastName ?? ""
-        }`,
+        userName: `${user.result?.firstName ?? ""} ${user.result?.lastName ?? ""
+          }`,
       };
       return JSON.stringify({ status: "success", data: data });
     } catch (err) {
@@ -45,111 +44,261 @@ export default component$(() => {
       ? parseFloat(order.totalPrice) - parseFloat(order.totalPrice) * 0.13
       : parseFloat(order.totalPrice) - 15 - parseFloat(order.totalPrice) * 0.13;
   const hst = parseFloat(order.totalPrice) * 0.13;
-
-  // subTotal.value = cartContext?.cart?.totalPrice ?? 0;
-  // hst.value = (cartContext?.cart?.totalPrice ?? 0) * 0.13;
-  // if (subTotal.value > 150) {
-  //   shipping.value = 0;
-  // } else {
-  //   shipping.value = 15;
-  // }
-  // total.value =
-  //   (cartContext?.cart?.totalPrice ?? 0) + hst.value + shipping.value;
+  const date = new Date(order.createdAt);
   return (
+
     <div class="card shadow-lg">
-      <div class="card-body justify-center items-center">
-        <div class="flex flex-col gap-2 justify-center items-center">
-          <h2 class="text-black text-xl font-semibold">Order Details</h2>
-          <p class="text-gray-600">
-            <span class="font-bold">Order No:</span> {order.order_number}
-          </p>
-          <p class="text-gray-600">
-            <span class="font-bold">Order Date:</span>{" "}
-            {new Date(order.createdAt).toLocaleString("en-US", {
-              timeZone: "America/Toronto",
-            })}
-          </p>
-          <div class="divider divider-vertical"></div>
-          <div class="flex flex-col justify-center items-center gap-2">
-            <span class="font-bold">Shipping Details:</span>{" "}
-            <span>{myOrderDetailsData.data.userName}</span>
-            <span>{order.shippingAddress.addressLine1}</span>
-            <span>{order.shippingAddress.postalCode}</span>
-            <span>{order.shippingAddress.city}</span>
-            <span>{order.shippingAddress.country}</span>
+      <div class="card-body justify-center ">
+        <div class="flex flex-col gap-2 justify-center">    
+          <div>
+            <span class="font-bold text-black text-xl uppercase">Order No: {order.order_number}</span>
+
           </div>
-          <div class="divider divider-vertical"></div>
-          <div class="flex flex-col justify-center items-center gap-2">
-            <span class="font-bold">Payment Details:</span>{" "}
-            <span class="text-sm">
-              <span class="font-bold">SubTotal:</span>{" "}
-              {subTotal.toLocaleString("en-US", {
-                style: "currency",
-                currency: "CAD",
-              })}
-            </span>
-            <span class="text-sm">
-              <span class="font-bold">HST:</span>{" "}
-              {hst.toLocaleString("en-US", {
-                style: "currency",
-                currency: "CAD",
-              })}
-            </span>
-            <span class="text-sm">
-              <span class="font-bold">Shipping:</span>{" "}
-              {parseFloat(order.totalPrice) > 150 ? "Free" : "CA$15.00"}
-            </span>
-            <span class="text-sm">
-              <span class="font-bold">Total:</span>{" "}
-              {order.totalPrice.toLocaleString("en-US", {
-                style: "currency",
-                currency: "CAD",
-              })}
-            </span>
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+                Order Number:
+
+              </span>
+            </div>
+            <div class="md:pt-6">
+              <span class="font-bold uppercase">
+                {order.order_number}
+              </span>
+
+            </div>
           </div>
-          <div class="divider divider-vertical"></div>
-          <div class="flex flex-col gap-2 justify-center items-center">
-            <span class="font-bold">Products Details:</span>{" "}
-            {order.products.map((product: any, index: number) => {
-              return (
-                <div
-                  class="card shadow-md justify-center items-center"
-                  key={index}
-                >
-                  <figure>
-                    <img
-                      src={product.product_img}
-                      alt={product.product_name}
-                      class="w-20 h-20"
-                    />
-                  </figure>
-                  <div class="card-body">
-                    <span class="text-sm">
-                      <span class="font-bold">Product Name:</span>{" "}
-                      {product.product_name}
-                    </span>
-                    <span class="text-sm">
-                      <span class="font-bold">Product Variation:</span>{" "}
-                      {product.variation_name ?? ""}
-                    </span>
-                    <span class="text-sm">
-                      <span class="font-bold">Product Price:</span>{" "}
-                      {product.price.toLocaleString("en-US", {
-                        style: "currency",
-                        currency: "CAD",
-                      })}
-                    </span>
-                    <span class="text-sm">
-                      <span class="font-bold">Product Quantity:</span>{" "}
-                      {product.quantity}
-                    </span>
-                  </div>
-                </div>
-              );
-            })}
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+                Order Date:
+
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+                {date.toLocaleString("en-US", {
+                  timeZone: "America/Toronto",
+                })}
+              </span>
+
+            </div>
           </div>
         </div>
+        <div class="flex flex-col gap-2 justify-center  py-8">
+          {/* <h2 class="text-black text-xl font-bold"> Order No: {order.order_number}</h2> */}
+          <div>
+            <span class="font-bold text-black text-xl ">Shipping Details: </span>
+
+          </div>
+
+
+
+
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+
+                Address: 
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+                <span>{order.shippingAddress.addressLine1} { " " }</span>
+                <span>{order.shippingAddress.postalCode} { " " }</span>
+                <span>{order.shippingAddress.city} { " " }</span>
+                <span>{order.shippingAddress.country} { " " }</span>
+              </span>
+
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="flex flex-col gap-2 justify-center  py-8">
+          {/* <h2 class="text-black text-xl font-bold"> Order No: {order.order_number}</h2> */}
+          <div>
+            <span class="font-bold text-black text-xl ">Payment Details:</span>
+
+          </div>
+
+
+
+
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+
+                Subtotal:
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+                {subTotal.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "CAD",
+                })}
+
+              </span>
+
+            </div>
+          </div>
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+
+                HST
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+                {hst.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "CAD",
+                })}
+
+              </span>
+
+            </div>
+          </div>
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+
+                Shipping:
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+                {parseFloat(order.totalPrice) > 150 ? "Free" : "CA$15.00"}
+
+              </span>
+
+            </div>
+          </div>
+          <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+            <div class="">
+              <span>
+
+                Total:
+              </span>
+            </div>
+            <div class="">
+              <span class="font-bold">
+
+                {order.totalPrice.toLocaleString("en-US", {
+                  style: "currency",
+                  currency: "CAD",
+                })}
+              </span>
+
+            </div>
+          </div>
+
+
+        </div>
+
+        <div class="flex flex-col gap-2 justify-center 
+        ">
+          {/* <h2 class="text-black text-xl font-bold"> Order No: {order.order_number}</h2> */}
+          <div>
+            <span class="font-bold text-black text-xl ">Product Details:</span>
+
+          </div>
+
+
+          {order.products.map((product: any, index: number) => {
+            return (
+              <div class="border border-[#ced4da] shadow-md rounded-md py-6
+              "
+              key={index}>
+
+                <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+                  <div class="">
+                    <span>
+
+                      Product Image:
+                    </span>
+                  </div>
+                  <div class="">
+                   
+                      <img
+                        src={product.product_img}
+                        alt={product.product_name}
+                        class="w-20 h-20"
+                      />
+                    
+
+                  </div>
+                </div>
+                <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+                  <div class="">
+                    <span>
+
+                      Product Name:
+                    </span>
+                  </div>
+                  <div class="">
+                    <span class="font-bold">
+                    {product.product_name}
+
+                    </span>
+
+                  </div>
+                </div>
+                <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+                  <div class="">
+                    <span>
+
+                      Product Variation:
+                    </span>
+                  </div>
+                  <div class="">
+                    <span class="font-bold">
+                      {product.variation_name}
+
+                    </span>
+
+                  </div>
+                </div>
+                <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+                  <div class="">
+                    <span>
+                      Product Price:
+                    </span>
+                  </div>
+                  <div class="">
+                    <span class="font-bold">
+
+                      CA${product.price}
+                    </span>
+
+                  </div>
+                </div>
+                <div class="grid grid-flow-row-dense gap-3 pl-6 md:grid-cols-4 grid-cols-2">
+                  <div class="">
+                    <span>
+                      Quanitity:
+                    </span>
+                  </div>
+                  <div class="">
+                    <span class="font-bold">
+
+                      
+                      {product.quantity}
+                    </span>
+
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+
       </div>
+
     </div>
   );
 });
