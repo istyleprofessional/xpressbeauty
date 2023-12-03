971cb9b289a4e8858348ac63c7dc738850b57132
ramyanber94 <ramyanber@gmail.com>
2023-12-03 01:50:05 -0500

    update google sheet

--- data.index.js
+++ data.index.js
@@ -8,41 +8,29 @@ const NEXT_APP_MONGO_URL = process.env.VITE_QWIK_APP_MONGO_CONNECTION;
 set("strictQuery", false);
 const mongoUrl = NEXT_APP_MONGO_URL || "";
 
-const changeCurrency = async () => {
+async function changeCurrency() {
   await connect(mongoUrl);
   const products = await Product.find({});
   for (const product of products) {
     if (product.priceType === "range") {
       for (const variant of product.variations) {
-        variant.price = (
-          Math.round(
-            parseFloat(variant?.price?.toString()?.replace("$", "")) * 100
-          ) / 100
-        ).toFixed(2);
+        variant.price =
+          parseFloat(variant?.price?.toString()?.replace("$", "")) *
+          (100).toFixed(2);
       }
-      product.price.max = (
-        Math.round(
-          parseFloat(product?.price?.max?.toString()?.replace("$", "")) * 100
-        ) / 100
-      ).toFixed(2);
+      product.price.max =
+        parseFloat(product?.price?.max?.toString()?.replace("$", "")) *
+        (100).toFixed(2);
       product.price.min = (
-        Math.round(
-          parseFloat(product?.price?.min?.toString()?.replace("$", "")) * 100
-        ) / 100
+        parseFloat(product?.price?.min?.toString()?.replace("$", "")) * 100
       ).toFixed(2);
     } else {
-      product.price.regular = (
-        Math.round(
-          parseFloat(product?.price?.regular?.toString()?.replace("$", "")) *
-            100
-        ) / 100
+      product.price.regular = parseFloat(
+        product?.price?.regular?.toString()?.replace("$", "")
       ).toFixed(2);
     }
     await product.save();
   }
-  console.log("done");
-  return;
-  // conn.disconnect();
-};
+}
 
 changeCurrency();
