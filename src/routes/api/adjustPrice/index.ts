649d529780c29d5db1e101288563da2c911167d0
ramyanber94 <ramyanber@gmail.com>
2023-12-09 03:38:49 -0500

    fix

--- src/routes/api/adjustPrice/index.ts
+++ src/routes/api/adjustPrice/index.ts
@@ -1,7 +1,9 @@
 import type { RequestHandler } from "@builder.io/qwik-city";
+import { connect } from "~/express/db.connection";
 import productSchema from "~/express/schemas/product.schema";
 
 export const onGet: RequestHandler = async ({ json }) => {
+  await connect();
   const products = await productSchema.find({}).lean();
   for (const product of products) {
     if (product.variations.length > 0) {
