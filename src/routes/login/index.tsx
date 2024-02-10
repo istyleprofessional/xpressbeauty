285823e1afb9f672ff8acf87c2bd6f8af5c96f8c
Ramy Anber <71028466+ramyanber94@users.noreply.github.com>
2024-02-10 16:11:26 -0500

    scrapping file created

--- src/routes/login/index.tsx
+++ src/routes/login/index.tsx
@@ -140,9 +140,8 @@ export default component$(() => {
           {" "}
         </div>
         <script
-          src={`https://www.google.com/recaptcha/api.js?render=${
-            import.meta.env.VITE_RECAPTCHA_SITE_KEY
-          }`}
+          src={`https://www.google.com/recaptcha/api.js?render=${import.meta.env.VITE_RECAPTCHA_SITE_KEY
+            }`}
         ></script>
         <Form action={action} reloadDocument={false}>
           <div class="card w-[90%] md:w-[35rem] h-fit m-6 shadow-xl bg-[#F4F4F5] flex flex-col justify-center items-center gap-5 p-5">
@@ -164,13 +163,43 @@ export default component$(() => {
               type="text"
               identifier="email"
             />
+            <div class="form-control w-full">
+              <label class="label">
+                <span class="label-text text-black text-base">
+                  Password <span class=" text-error">*</span>
+                </span>
+              </label>
+              <div class="relative w-full">
+                <div class="absolute inset-y-0 right-0 flex items-center px-2">
+                  <input onChange$={(e: any) => {
+                    const input = e.target;
+                    console.log(input.checked);
+                    const password = document.querySelector('.js-password');
+                    const button = document.querySelector('.js-password-label');
+                    if (!password || !button) return;
+                    if (input.checked) {
+                      password.setAttribute('type', 'text');
+                      button.textContent = 'hide';
+                    } else {
+                      password.setAttribute('type', 'password');
+                      button.textContent = 'show';
+                    }
+                  }} class="hidden js-password-toggle" id="toggle" type="checkbox" />
+                  <label class="bg-gray-300 hover:bg-gray-400 rounded px-2 py-1 text-sm text-gray-600 font-mono cursor-pointer js-password-label" for="toggle">show</label>
+                </div>
+                <input name="password" class={`input input-bordered w-full text-black js-password 
+                ${action.value?.validation?.password === false ? 'input-error' : ""}`} id="password" type="password" autoComplete="off"
+                />
+              </div>
+            </div>
+            {/* 
             <InputField
               label="Password"
               placeholder="**********"
               validation={action?.value?.validation?.password}
               type="password"
               identifier="password"
-            />
+            /> */}
             <input
               type="hidden"
               name="recaptcha"
@@ -203,9 +232,8 @@ export default component$(() => {
                 Forget Password ?
               </button>
               <a
-                class={`btn btn-ghost text-black ${
-                  recaptchaToken.value.length === 0 ? "pointer-events-none" : ""
-                }`}
+                class={`btn btn-ghost text-black ${recaptchaToken.value.length === 0 ? "pointer-events-none" : ""
+                  }`}
                 href="/register"
               >
                 Don't have an account? Register Now
