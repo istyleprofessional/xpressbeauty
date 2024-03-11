8f39c55e5a86a7a601d99f58245b6613b7eaba54
ramyanber94 <ramyanber@gmail.com>
2024-03-11 11:26:48 -0400

    sign in with google

--- src/express/schemas/users.schema.ts
+++ src/express/schemas/users.schema.ts
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     password: { type: String },
     firstName: { type: String, default: null },
     lastName: { type: String, default: null },
-    phoneNumber: { type: String },
+    phoneNumber: { type: String, default: null },
     generalInfo: { type: Object, default: null },
     EmailVerifyToken: { type: String },
     PhoneVerifyToken: { type: String },
