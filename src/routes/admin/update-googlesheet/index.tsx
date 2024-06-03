4a46c6a8316a3e46c0b4411a728dca29743fc029
ramyanber94 <ramyanber@gmail.com>
2024-06-03 15:01:32 -0400

    done

--- src/routes/admin/update-googlesheet/index.tsx
+++ src/routes/admin/update-googlesheet/index.tsx
@@ -174,7 +174,6 @@ export default component$(() => {
         disabled={isLoading.value}
         class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         onClick$={async () => {
-          debugger;
           isLoading.value = true;
           const update = await updateGoogleSheet();
           const updateJson = JSON.parse(update);
